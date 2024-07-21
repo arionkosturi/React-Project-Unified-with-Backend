bb35a0a5b4d073f018e7793f467f91577d60bcc0
arionkosturi <arionkosturi@gmail.com>
2024-07-21 21:12:30 +0200

    fixed highlighted section in frontend

--- client/src/frontend/HighlitedSection.js
+++ client/src/frontend/HighlitedSection.js
@@ -1,39 +1,45 @@
 // @ts-nocheck
-
 import {
   useFetchHighlightedArticles,
   useFetchHighlightedArticle,
 } from "../components/hooks/useFetchArticles";
-import React, { useState, useEffect, Component } from "react";
-import { useQuery } from "@tanstack/react-query";
+import React, { useState } from "react";
 function HighlitedSection() {
   const { data: articles, status } = useFetchHighlightedArticles();
-  let [artikull, setArtikull] = useState({
-    title: "test",
-    imgUrl: "",
-  });
-  // const { data } = useQuery({
-  //   queryKey: ["first"],
-  //   queryFn: () =>
-  //     fetch("http://localhost:3344/news/top1").then((res) => res.json()),
-  // });
   const { data: firstArt, isSuccess } = useFetchHighlightedArticle();
-  // firstArt.values;
-  // console.log(Object.entries(firstArt));
-  // console.log(Object.values(firstArt.values));
-  isSuccess && console.log(firstArt[0].title);
+  let [artikull, setArtikull] = useState({});
+
   return (
     <div className="md:flex mx-1">
       {/* Left Side */}
       <div className="relative hover:opacity-100 my-2 ml-1 md:ml-4 xl:ml-0 md:w-2/3 hover:cursor-pointer">
-        <div className="opacity-95 hover:opacity-100 mr-2">
-          <a href="article.html?id=/">
-            <img src={artikull.imgUrl} alt="" className="opacity-90" />
-            <div className="relative bg-purple-800 bg-opacity-100 lg:bg-opacity-80 hover:bg-opacity-90 lg:-mt-28 py-2 w-full h-28 text-white">
-              <h3 className="p-1 md:text-xl line-clamp-2 ">{artikull.title}</h3>
-            </div>
-          </a>
-        </div>
+        {status === "success" ? (
+          <div className="opacity-95 hover:opacity-100 mr-2">
+            <a href="article.html?id=/">
+              <img
+                src={artikull.imgUrl || (firstArt && firstArt[0]?.imgUrl)}
+                alt=""
+                className="opacity-90"
+              />
+              <div className="relative bg-purple-800 bg-opacity-100 lg:bg-opacity-80 hover:bg-opacity-90 lg:-mt-28 py-2 w-full h-28 text-white">
+                <h3 className="p-1 md:text-xl line-clamp-2 ">
+                  {artikull.title || (firstArt && firstArt[0]?.title)}
+                </h3>
+              </div>
+            </a>
+          </div>
+        ) : (
+          <div className="opacity-95 hover:opacity-100 mr-2">
+            <a href="article.html?id=/">
+              <img src={artikull.imgUrl} alt="" className="opacity-90" />
+              <div className="relative bg-purple-800 bg-opacity-100 lg:bg-opacity-80 hover:bg-opacity-90 lg:-mt-28 py-2 w-full h-28 text-white">
+                <h3 className="p-1 md:text-xl line-clamp-2 ">
+                  {artikull.title}
+                </h3>
+              </div>
+            </a>
+          </div>
+        )}
       </div>
       {/* Right Side */}
       <div className=" flex-row my-1 p-2 w-full md:w-1/2 lg:w-1/3 hover:cursor-pointer">
