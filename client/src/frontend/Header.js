fba4bd2f0b5372c60389eec0767700f8ddbcd723
arionkosturi <arionkosturi@gmail.com>
2024-08-02 18:26:36 +0200

    fixed minor bug: z-index of header element

--- client/src/frontend/Header.js
+++ client/src/frontend/Header.js
@@ -69,7 +69,7 @@ export default function Header() {
           </label>
           <input type="checkbox" id="menu-toggler" className="hidden peer" />
 
-          <nav className="container mx-auto peer-checked:block  z-30 xl:relative top-16 xl:top-0 absolute xl:flex justify-start lg:items-center hidden bg-white  shadow-md lg:shadow-none sm:mt-0 xl:mr-10 py-2 w-full">
+          <nav className="container mx-auto peer-checked:block xl:relative top-16 xl:top-0 absolute xl:flex justify-start lg:items-center hidden bg-white  shadow-md lg:shadow-none sm:mt-0 xl:mr-10 py-2 w-full">
             <div className="flex xl:flex-row flex-col shadow-md xl:shadow-none mx-2 px-2 text-left text-purple-700">
               <div className=" xl:relative top-16 xl:top-0 flex flex-col md:flex-row justify-start md:items-left bg-white lg:shadow-none sm:mt-0 xl:mr-10 py-2 w-full">
                 <div className="xl:relative top-16 xl:top-0 flex flex-col lg:flex-row justify-start md:items-left bg-white   lg:shadow-none sm:mt-0 xl:mr-10 py-2 w-full">
